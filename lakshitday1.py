6a7f1222466c237dac1c971480c822038e1a1d2d
Beingwizard <lakshitpant760@gmail.com>
2024-04-06 04:47:44 +0530

    Day1ofLakshit

---
+++ lakshitday1.py
@@ -0,0 +1,61 @@
+# print("Lakshit Pant")
+# print('0-------')
+# print('  ||||||')
+# print('*' * 10)
+
+# #Variables
+# price =10
+# # print(price)
+# rating = 4.9
+# # print(rating)
+# name='Lakshit'
+# is_published = True
+# name = "Josh"
+# age = 20
+# is_new = True
+# name =input("Add your text here: ")
+# print = name
+# name=input("Add your name here:")
+# color=input("Add your favorite color here:")
+# print(name +' Favorite color is '+color)
+# birth_year = int(input('Birth year:'))
+# age = 2024-birth_year
+# print(age)
+# print(type(age))
+# w_in_pounds = int(input("Enter your weight in pounds: "))
+# w_in_kgs = int(w_in_pounds) * 0.45
+# print(w_in_kgs)
+# course="Python is beginners"
+# print(course[0:8])
+# print(course[:-8])
+# first ='John'
+# last ='Smith'
+# message = f'{first} {last} is a coder'
+# print(message)
+# print(len(message))
+# message = 'I am a coder'
+# # print(message.upper())
+# print(message.find('P'))
+# print(message.replace('coder', 'loser'))
+# x=10
+# x -=3
+# print(x)
+# import math
+# x=2.9
+# print(math.floor(x))
+# print(math.ceil(x))
+# print(round(x))
+# print(abs(x))
+# Day = input("What's the day look like: ")
+# if Day == 'Hot':
+#     print("It's a hot Day here, drink plenty of water")
+# elif Day == 'Cold':
+#         print("It's a cold day here, wear warm clothes")
+# else:
+#         print("It's a lovely day") 
+Credit=int(input("What's the mininum credit amt you can pay : "))
+if Credit > 300000:
+    print("You've to put down 10%")
+else:
+    print("You've to put down to 20%")
+print("Wht's your down payment")
